4d0ed1cdf1cc15a042d954db4816c5fcc4d76c1f
jkono <j@je>
2023-05-11 00:20:25 -0500

    Map tiles not showing, but otherwise vid 11 complete...

--- client/src/components/StateMap.vue
+++ client/src/components/StateMap.vue
@@ -8,6 +8,14 @@
     <p v-else>You've not yet visited this state!</p>
 
     <!-- insert map here -->
+    <div id="map-container">
+        <l-map v-bind:center="mapCenter" v-bind:zoom="state.zoom">
+            <l-tile-layer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution:="http://www.openstreetmap.org/copyright">
+            </l-tile-layer>
+        </l-map>
+    </div>
 
 </div>
 
@@ -16,10 +24,11 @@
 
 <script>
 
-
+import {LMap, LTileLayer} from "@vue-leaflet/vue-leaflet";
 
 export default {
     name: 'StateMap',
+    components: {LMap,LTileLayer},
     data(){
         return {
             state: {} // works with mounted this.state.name to grab the state from the route parameter defined in @/router/index.js's routes
@@ -42,6 +51,6 @@ export default {
 
 <style scoped>
 
-
+#map-container {height: 30rem}
 
 </style>
